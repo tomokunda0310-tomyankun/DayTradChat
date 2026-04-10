ae29adb259152a506dcf2714d9dd957133e43759
tomokunda0310-tomyankun <tomokunda0310@gmail.com>
2026-04-10 00:58:26 +0000

    init: DayTradeChat minimal client

---
+++ app/src/main/java/com/daytradchat/papa/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.daytradchat.papa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
